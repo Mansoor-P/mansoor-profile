df3391637444de0154e7d7569b15d65e23a80b92
Mansoor-P <munaf6007@gmail.com>
2025-03-05 15:45:18 +0530

    Updated Home component: temporarily removed GitHubProjects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import Hero from "../components/Hero";
 import Skills from "../components/Skills";
 import Blog from "./Blog";
-import GitHubProjects from "./GithubProjects";
 const Home = () => {
   return (
     <section className="px-6 lg:px-10 py-10 max-w-6xl mx-auto space-y-12">
-      <Hero/>
+      <Hero />
       <Skills />
       <Blog />
-      {/* <GitHubProjects /> */}
     </section>
   );
 };
